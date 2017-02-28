0ee4318f6d49982ff7739b4d8e5842cf7fbfe5eb
Chris Robertson <chrisxrobertson@gmail.com>
2017-02-28 16:54:39 +1100

    Enemy now pulls name randomly from a YAML file.

--- lib/zarta/enemy.rb
+++ lib/zarta/enemy.rb
@@ -4,8 +4,10 @@ module Zarta
   class Enemy
     # Enemy's name
     attr_accessor :name
+
     def initialize
-      @name = 'Test Badguy'
+      @enemy_list = YAML.load_file(__dir__ + '/enemy.yml')
+      @name       = @enemy_list[rand(0...@enemy_list.length)]
     end
   end
 end

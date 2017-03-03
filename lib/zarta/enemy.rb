2bb1455fd665facc3dca93795bf5484ef874c1e9
Chris Robertson <chrisxrobertson@gmail.com>
2017-03-03 15:27:52 +1100

    Merging in major refactoring.

--- lib/zarta/enemy.rb
+++ lib/zarta/enemy.rb
@@ -2,39 +2,34 @@
 module Zarta
   # The enemy class
   class Enemy
-    # The enemi's name
+    # The enemy's name
     attr_accessor :name
-
-    # The enemie's health in an array
-    # [current health / max health]
-    attr_accessor :health
 
     # The enemy's current experience level
     attr_accessor :level
 
-    # The enemy's description
-    attr_accessor :description
-
     # The enemy's current weapon
     attr_accessor :weapon
 
+    # Did you kill him? Or did you run away? You probably ran away...
+    attr_accessor :dealt_with
+
     def initialize(dungeon)
       @dungeon = dungeon
+      @dealt_with = false
+      @player = @dungeon.player
 
       pick_enemy
       set_level
-      set_health
-      pick_weapon
-    end
-
-    def pick_weapon
+      @health = set_health
       @weapon = Zarta::Weapon.new(@dungeon)
-      @weapon.random_drop
+      @pastel = Pastel.new
     end
 
     def pick_enemy
       return if boss_spawn
       spawn_list = []
+      chance = spawn_chance
       @dungeon.enemy_list.each do |enemy|
         spawn_list << enemy if enemy[:rarity] <= chance
       end
@@ -46,26 +41,29 @@ module Zarta
       @rarity = spawn[:rarity]
     end
 
-    def chance
-      (@dungeon.player.level + @dungeon.level) / 2
+    def spawn_chance
+      rand(1..(@player.level + @dungeon.level + 2))
     end
 
     def set_level
       plevel = @dungeon.player.level
-      @level = rand((plevel - 2)..(plevel + @rarity))
+      @level = rand((plevel - 2)..(plevel + @rarity / 2))
     end
 
     def set_health
       base = @rarity + @level + @dungeon.level + @dungeon.player.level
-      current_health = rand(base..base * 3)
+      current_health = rand(base..base * 2)
       max_health = current_health
-      @health = [current_health, max_health]
+      [current_health, max_health]
     end
 
     def take_damage(damage)
       @health[0] -= damage
-      return true if @health[0] <= 0
-      false
+      return unless @health[0] <= 0
+      # The enemy drops its weapon when killed. This will overwrite any weapon
+      # that may have spawned in the room.
+      @dungeon.room.weapon = @weapon
+      @dealt_with = true
     end
 
     def boss_spawn
@@ -76,28 +74,54 @@ module Zarta
       @description = 'The BOSS!'
       @rarity = 12
 
-      @level = rand(@dungeon.player.level..(@dungeon.player.level * 1.5))
+      set_level
       set_health
       pick_weapon
       true
     end
 
+    def enemy_turn
+      enemy_hit = enemy_damage
+      @player.take_damage(enemy_hit)
+      puts "The #{@pastel.magenta.bold(@name)} hits you!"
+      puts "You take #{@pastel.red.bold(enemy_hit)} damage."
+      gets
+    end
+
+    # This function and the player one should be moved into a single function.
+    # Just pass in the player or enemy.
+    def enemy_damage
+      base_damage = @weapon.damage + rand(@weapon.damage)
+      hit = base_damage + rand(@level) + @level
+      hit.round
+    end
+
     def inspect
       Zarta::HUD.new(@dungeon)
+<<<<<<< HEAD
       @pastel = Pastel.new
+=======
+      table_title
+      table
+      gets
+    end
+
+    def table_title
+>>>>>>> major-refactor
       table_title = Terminal::Table.new
       table_title.title = @pastel.magenta.bold(@name)
       table_title.style = { width: 40, padding_left: 1 }
       table_title.align_column(0, :center)
       table_title.add_row [@description]
       puts table_title
+    end
 
+    def table
       table = Terminal::Table.new
       table.style = { width: 40, padding_left: 1 }
       table.add_row ['Weapon:', @weapon.name]
       table.add_row ['Health:', "#{@health[0]}/#{@health[1]}"]
       puts table
-      gets
     end
   end
 end
